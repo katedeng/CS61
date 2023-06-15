9f2b727e6f91767f90f8005d323cbde1d3b065aa
kate deng <katedeng2020@gmail.com>
2023-06-14 22:47:55 -0600

    navie AList

--- HW0/AList.java
+++ HW0/AList.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class AList {
+package PACKAGE_NAME;
+public class AList {
+    int[] items;
+    int size;
+    /** creates an empty list.*/
+    public AList(){
+        items = new int[100];
+        size = 0;
+    }
+
+    /** insert x into the back of the list*/
+    public void addLast(int x){
+        items[size] = x;
+        size = size + 1;
+    }
+
+    /** returns the item from the back of the list */
+    public int getLast(){
+        return items[size-1];
+    }
+
+    /** gets the ith item in the list (0 is the front) */
+    public int get(int i){
+        return items[i];
+    }
+
+    /** returns the number of items in the list */
+    public int size(){
+        return size;
+    }
+
+
+
 }
---
+++ HW0/AListTest.java
@@ -0,0 +1,7 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+/** Test the AList class. */
+public class AListTest {
+    @Test
+
+}
